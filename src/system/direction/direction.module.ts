28f1068082687c0075c3df69f48b947c61006667
JulienZeng <936636589@qq.com>
2024-05-08 22:16:46 +0800

    修复无法找到模块的问题

--- src/system/direction/direction.module.ts
+++ src/system/direction/direction.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DirectionService } from './direction.service';
 import { DirectionController } from './direction.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Direction } from './entities/direction.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Direction])],
   controllers: [DirectionController],
   providers: [DirectionService],
+  exports: [DirectionService],
 })
 export class DirectionModule {}
