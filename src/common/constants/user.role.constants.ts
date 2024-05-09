887fb78da2b50ecb932c9d234fa3cdd666af90e8
JulienZeng <936636589@qq.com>
2024-05-09 21:24:52 +0800

    定义全局固定变量

---
+++ src/common/constants/user.role.constants.ts
@@ -0,0 +1,16 @@
+export const USER_ROLE_LEVEL = {
+  // 成员
+  MEMBER: 1,
+  // 副主任
+  DEPUTY_DIRECTOR: 2,
+  // 主任
+  DIRECTOR: 3,
+  // 副总监
+  DEPUTY_MANAGER: 4, //总监
+  // 总监
+  MANAGER: 5,
+  // 理事
+  COUNCIL_MEMBER: 6,
+  // 管理员
+  ADMINISTRATOR: 100,
+};
