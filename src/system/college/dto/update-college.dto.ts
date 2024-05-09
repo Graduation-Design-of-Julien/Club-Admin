1ef4d61b2d3c01109f82f5f1de9f150f3af6260b
JulienZeng <936636589@qq.com>
2024-05-09 11:37:35 +0800

    fix：修复提示错误

--- src/system/college/dto/update-college.dto.ts
+++ src/system/college/dto/update-college.dto.ts
@@ -3,9 +3,9 @@ import { CreateCollegeDto } from './create-college.dto';
 import { IsNotEmpty } from 'class-validator';
 
 export class UpdateCollegeDto extends PartialType(CreateCollegeDto) {
-  @IsNotEmpty({ message: '部门ID不能为空。' })
+  @IsNotEmpty({ message: '学院ID不能为空。' })
   collegeCode: number;
 
-  @IsNotEmpty({ message: '部门名称不能为空。' })
+  @IsNotEmpty({ message: '学院名称不能为空。' })
   collegeName: string;
 }
