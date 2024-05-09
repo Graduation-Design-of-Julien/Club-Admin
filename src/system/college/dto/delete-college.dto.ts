73619a4419fe808365f8c56271e7383abcc6b7a1
JulienZeng <936636589@qq.com>
2024-05-09 11:55:31 +0800

    修改删除方法的逻辑

--- src/system/college/dto/delete-college.dto.ts
+++ src/system/college/dto/delete-college.dto.ts
@@ -4,9 +4,6 @@ export class DeleteCollegeDto {
   @IsNotEmpty({ message: '学院ID不能为空。' })
   collegeCode: number;
 
-  @IsNotEmpty({ message: '学院名称不能为空。' })
-  collegeName: string;
-
   @IsNotEmpty({ message: '删除标记不能为空' })
   deleted: number;
 }
--- src/system/department/dto/delete-department.dto.ts
+++ src/system/department/dto/delete-department.dto.ts
@@ -4,9 +4,6 @@ export class DeleteDepartmentDto {
   @IsNotEmpty({ message: '部门ID不能为空。' })
   departmentCode: number;
 
-  @IsNotEmpty({ message: '部门名称不能为空。' })
-  departmentName: string;
-
   @IsNotEmpty({ message: '删除标记不能为空' })
   deleted: number;
 }
