e0fdbf789e0910664645821ca0e3d8365a0d2f56
JulienZeng <936636589@qq.com>
2024-05-09 21:32:46 +0800

    完成ORM连接，添加各个模块，导入拦截器

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './system/auth/auth.module';
+import { DepartmentModule } from './system/department/department.module';
+import { DirectionModule } from './system/direction/direction.module';
+import { RoleModule } from './system/role/role.module';
+import { CollegeModule } from './system/college/college.module';
+import { MojarModule } from './system/mojar/mojar.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConfig } from './common/utils/configLoader';
+import { UserModule } from './system/user/user.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './common/guards/auth.guard';
+
+// load mysql config
+const { MYSQL_CONFIG } = getConfig();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...MYSQL_CONFIG,
+      synchronize: true,
+      retryDelay: 509,
+      retryAttempts: 10,
+      autoLoadEntities: true,
+    }),
+    AuthModule,
+    DepartmentModule,
+    DirectionModule,
+    RoleModule,
+    CollegeModule,
+    MojarModule,
+    UserModule,
+  ],
+  providers: [{ provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import { TransformInterceptor } from './common/interceptor/transform.interceptor';
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 全局路由前缀
+  app.setGlobalPrefix('cms');
+  // 统一响应体格式
+  app.useGlobalInterceptors(new TransformInterceptor());
+  // 自动验证请求参数
+  app.useGlobalPipes(new ValidationPipe());
+  // 全局异常过滤器
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+
+  // 使用session
+  app.use(
+    session({
+      secret: 'cms-secret',
+      resave: false,
+      saveUninitialized: false,
+      rolling: true,
+    }),
+  );
+
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
